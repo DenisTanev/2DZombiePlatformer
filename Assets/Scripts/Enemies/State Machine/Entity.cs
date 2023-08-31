29552d4181e95bdc27ae8357bf65848b34104452
DenisTanev <denistanev@icloud.com>
2023-08-31 20:08:33 +0300

    Improved the enemy, now it can die and other few tweaks

--- Assets/Scripts/Enemies/State Machine/Entity.cs
+++ Assets/Scripts/Enemies/State Machine/Entity.cs
@@ -10,19 +10,31 @@ public class Entity : MonoBehaviour
 
     public int facingDirection { get; private set; }
 
+    public int lastDamageDirection { get; private set; }
+
     public Rigidbody2D rb { get; private set; }
     public Animator anim { get; private set; }
     public GameObject aliveGO { get; private set; }
 
     public AnimationToStateMachine atsm { get; private set; }
 
-    [SerializeField] private Transform wallCheck, ledgeCheck, playerCheck;
+    [SerializeField] private Transform wallCheck, ledgeCheck, playerCheck, groundCheck;
 
     private Vector2 velocityWorkspace;
+
+    private float currentHealth;
+    private float currentStunResistance;
+    private float lastDamageTime;
+
+    protected bool isStunned;
+    protected bool isDead;
+
 
     public virtual void Start()
     {
         facingDirection = 1;
+        currentHealth = entityData.maxHealth;
+        currentStunResistance = entityData.stunResistance;
 
         aliveGO = transform.Find("Alive").gameObject;
         rb = aliveGO.GetComponent<Rigidbody2D>();
@@ -35,6 +47,11 @@ public class Entity : MonoBehaviour
     public virtual void Update()
     {
         stateMachine.currentState.LogicUpdate();
+
+        if (Time.time >= lastDamageTime + entityData.stunRecoveryTime)
+        {
+            ResetStunResistance();
+        }
     }
 
     public virtual void FixedUpdate()
@@ -45,6 +62,13 @@ public class Entity : MonoBehaviour
     public virtual void SetVelocity(float velocity)
     {
         velocityWorkspace.Set(facingDirection * velocity, rb.velocity.y);
+        rb.velocity = velocityWorkspace;
+    }
+
+    public virtual void SetVelocity(float velocity, Vector2 angle, int direction)
+    {
+        angle.Normalize();
+        velocityWorkspace.Set(angle.x * velocity * direction, angle.y * velocity);
         rb.velocity = velocityWorkspace;
     }
 
@@ -68,9 +92,57 @@ public class Entity : MonoBehaviour
         return Physics2D.Raycast(playerCheck.position, aliveGO.transform.right, entityData.maxAgroDistance, entityData.whatIsPlayer);
     }
 
+    public virtual void DamageHop(float velocity)
+    {
+        velocityWorkspace.Set(rb.velocity.x, velocity);
+        rb.velocity = velocityWorkspace;
+    }
+
     public virtual bool CheckPlayerInCloseRangeAction()
     {
         return Physics2D.Raycast(playerCheck.position, aliveGO.transform.right, entityData.closeRangeActionDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual bool CheckGround()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, entityData.groundCheckRadius, entityData.whatIsGround);
+    }
+
+    public virtual void ResetStunResistance()
+    {
+        isStunned = false;
+        currentStunResistance = entityData.stunResistance;
+    }
+
+    public virtual void Damage(AttackDetails attackDetails)
+    {
+        lastDamageTime = Time.time;
+
+        currentHealth -= attackDetails.damageAmount;
+        currentStunResistance -= attackDetails.stunDamageAmount;
+
+        DamageHop(entityData.damageHopSpeed);
+
+        Instantiate(entityData.hitParticle, aliveGO.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+
+        if (attackDetails.position.x > aliveGO.transform.position.x)
+        {
+            lastDamageDirection = -1;
+        }
+        else
+        {
+            lastDamageDirection = 1;
+        }
+
+        if (currentStunResistance <= 0)
+        {
+            isStunned = true;
+        }
+
+        if (currentHealth <= 0)
+        {
+            isDead = true;
+        }
     }
 
     public virtual void Flip()
