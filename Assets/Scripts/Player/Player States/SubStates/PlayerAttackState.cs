9d352bf3b2cb4c35f393bcdbc15520826154f135
DenisTanev <denistanev@icloud.com>
2023-09-10 19:31:07 +0300

    Worked on the sword attack

--- Assets/Scripts/Player/Player States/SubStates/PlayerAttackState.cs
+++ Assets/Scripts/Player/Player States/SubStates/PlayerAttackState.cs
@@ -6,6 +6,13 @@ public class PlayerAttackState : PlayerAbilityState
 {
     private Weapon weapon;
 
+    private int xInput;
+
+    private float velocityToSet;
+
+    private bool setVelocity;
+    private bool shouldCheckFlip;
+
     public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
     }
@@ -13,6 +20,8 @@ public class PlayerAttackState : PlayerAbilityState
     public override void Enter()
     {
         base.Enter();
+
+        setVelocity = false;
 
         weapon.EnterWeapon();
     }
@@ -24,14 +33,44 @@ public class PlayerAttackState : PlayerAbilityState
         weapon.ExitWeapon();
     }
 
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        xInput = player.InputHandler.NormalizedInputX;
+
+        if (shouldCheckFlip)
+        {
+            player.CheckIfShouldFlip(xInput);
+        }
+
+        if (setVelocity)
+        {
+            player.SetVelocityX(velocityToSet * player.FacingDirection);
+        }
+    }
+
     public void SetWeapon(Weapon weapon)
     {
         this.weapon = weapon;
         weapon.InitializeWeapon(this);
     }
 
+    public void SetPlayerVelocity(float velocity)
+    {
+        player.SetVelocityX(velocity * player.FacingDirection);
+
+        velocityToSet = velocity;
+        setVelocity = true;
+    }
+
+    public void SetFlipCheck(bool value)
+    {
+        shouldCheckFlip = value;
+    }
+
     #region Animation Triggers
-    
+
     public override void AnimationFinishTrigger()
     {
         base.AnimationFinishTrigger();
@@ -40,4 +79,6 @@ public class PlayerAttackState : PlayerAbilityState
     }
 
     #endregion
+
+    
 }
