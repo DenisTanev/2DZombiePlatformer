da3e2d89c5638f248c7b8967b643d4995df3c9a5
DenisTanev <denistanev@icloud.com>
2023-09-13 22:45:07 +0300

    Added a Stat core component

--- Assets/Scripts/Core/Core Components/CoreComponent.cs
+++ Assets/Scripts/Core/Core Components/CoreComponent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoreComponent : MonoBehaviour
+public class CoreComponent : MonoBehaviour, ILogicUpdate
 {
     protected Core core;
 
@@ -14,5 +14,12 @@ public class CoreComponent : MonoBehaviour
         {
             Debug.LogError("There is no Core on the parent");
         }
+
+        core.AddComponent(this);
+    }
+
+    public virtual void LogicUpdate()
+    {
+
     }
 }
--- Assets/Scripts/Enemies/UI/FloatingHealthBar.cs
+++ Assets/Scripts/Enemies/UI/FloatingHealthBar.cs
@@ -7,14 +7,8 @@ public class FloatingHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
 
-
     public void UpdateHealthBar(float currentValue, float maxValue)
     {
         slider.value = currentValue / maxValue;
-    }
-
-    void Update()
-    {
-        
     }
 }
