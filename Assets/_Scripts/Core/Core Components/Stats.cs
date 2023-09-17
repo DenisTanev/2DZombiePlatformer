a3f445bdf783c8fb37bb9662e74e426288e4e544
DenisTanev <denistanev@icloud.com>
2023-09-17 09:37:29 +0300

    Added Death

---
+++ Assets/_Scripts/Core/Core Components/Stats.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Stats : CoreComponent
+{
+    public event Action OnHealthZero;
+
+    [SerializeField] private float maxHealth;
+    [SerializeField] FloatingHealthBar healthBar;
+
+    private float currentHealth;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        currentHealth = maxHealth;
+
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+    }
+
+    public void DecreaseHealth(float amount)
+    {
+        currentHealth -= amount;
+        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+
+            OnHealthZero?.Invoke();
+        }
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    }
+}
---
+++ Assets/_Scripts/Enemies/State Machine/Entity.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    private Movement Movement { get => movement ?? Core.GetCoreComponent(ref movement); }
+
+    private Movement movement;
+
+    public FiniteStateMachine stateMachine;
+
+    public D_Entity entityData;
+
+    public int lastDamageDirection { get; private set; }
+
+    public Animator anim { get; private set; }
+
+    public AnimationToStateMachine atsm { get; private set; }
+
+    public Core Core { get; private set; }
+
+    [SerializeField] private Transform wallCheck, ledgeCheck, playerCheck, groundCheck;
+    [SerializeField] FloatingHealthBar healthBar;
+
+    private Vector2 velocityWorkspace;
+
+    public float currentHealth;
+    private float currentStunResistance;
+    private float lastDamageTime;
+
+    protected bool isStunned;
+    protected bool isDead;
+
+
+    public virtual void Awake()
+    {
+        Core = GetComponentInChildren<Core>();
+
+        currentHealth = entityData.maxHealth;
+        currentStunResistance = entityData.stunResistance;
+
+        anim = GetComponent<Animator>();
+        atsm = GetComponent<AnimationToStateMachine>();
+
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+        healthBar.UpdateHealthBar(currentHealth, entityData.maxHealth);
+
+        stateMachine = new FiniteStateMachine();
+    }
+    
+    public virtual void Update()
+    {
+        Core.LogicUpdate();
+        stateMachine.currentState.LogicUpdate();
+
+        anim.SetFloat("yVelocity", Movement.RB.velocity.y);
+
+        if (Time.time >= lastDamageTime + entityData.stunRecoveryTime)
+        {
+            ResetStunResistance();
+        }
+    }
+
+    public virtual void FixedUpdate()
+    {
+        stateMachine.currentState.PhysicsUpdate();
+    }
+
+    public virtual bool CheckPlayerInMinAgroRange()
+    {
+        return Physics2D.Raycast(playerCheck.position, transform.right, entityData.minAgroDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual bool CheckPlayerInMaxAgroRange()
+    {
+        return Physics2D.Raycast(playerCheck.position, transform.right, entityData.maxAgroDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual void DamageHop(float velocity)
+    {
+        velocityWorkspace.Set(Movement.RB.velocity.x, velocity);
+        Movement.RB.velocity = velocityWorkspace;
+    }
+
+    public virtual bool CheckPlayerInCloseRangeAction()
+    {
+        return Physics2D.Raycast(playerCheck.position, transform.right, entityData.closeRangeActionDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual void ResetStunResistance()
+    {
+        isStunned = false;
+        currentStunResistance = entityData.stunResistance;
+    }
+
+    public virtual void OnDrawGizmos()
+    {
+        if (Core != null)
+        {
+            Gizmos.DrawLine(wallCheck.position, wallCheck.position + (Vector3)(Vector2.right * Movement.FacingDirection * entityData.wallCheckDistance));
+            Gizmos.DrawLine(ledgeCheck.position, ledgeCheck.position + (Vector3)(Vector2.down * entityData.ledgeCheckDistance));
+
+            Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.closeRangeActionDistance), 0.2f);
+            Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.minAgroDistance), 0.2f);
+            Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.maxAgroDistance), 0.2f);
+        }
+    }
+}
