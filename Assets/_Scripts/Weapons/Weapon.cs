bb1daa232e7ffe560981ce0c1673a0f6b8c44287
DenisTanev <denistanev@icloud.com>
2023-09-17 19:38:56 +0300

    Added an attack timer which goes back to the first attack after certain amount of time has passed

--- Assets/_Scripts/Weapons/Weapon.cs
+++ Assets/_Scripts/Weapons/Weapon.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using OriginalFolderName.Utilities;
 using UnityEngine;
 
 namespace _Scripts.Weapons
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] private int numberOfAttacks;
+        [SerializeField] private float attackCounterResetCooldown;
+
+        public int CurrentAttackCounter
+        {
+            get => currentAttackCounter;
+            private set => currentAttackCounter = value >= numberOfAttacks ? 0 : value; 
+        }
+
         public event Action OnExit;
 
         private Animator anim;
@@ -14,16 +24,26 @@ namespace _Scripts.Weapons
 
         private AnimationEventHandler eventHandler;
 
+        private int currentAttackCounter;
+
+        private Timer attackCounterResetTimer;
+
         public void Enter()
         {
             print($"{transform.name} enter");
 
+            attackCounterResetTimer.StopTimer();
+
             anim.SetBool("active", true);
+            anim.SetInteger("counter", CurrentAttackCounter);
         }
 
         private void Exit()
         {
             anim.SetBool("active", false);
+
+            CurrentAttackCounter++;
+            attackCounterResetTimer.StartTimer();
 
             OnExit?.Invoke();
         }
@@ -34,16 +54,27 @@ namespace _Scripts.Weapons
             anim = baseGameObject.GetComponent<Animator>();
 
             eventHandler = baseGameObject.GetComponent<AnimationEventHandler>();
+
+            attackCounterResetTimer = new Timer(attackCounterResetCooldown);
         }
+
+        private void Update()
+        {
+            attackCounterResetTimer.Tick();
+        }
+
+        private void ResetAttackCounter() => CurrentAttackCounter = 0;
 
         private void OnEnable()
         {
             eventHandler.OnFinish += Exit;
+            attackCounterResetTimer.OnTimerDone += ResetAttackCounter;
         }
 
         private void OnDisable()
         {
             eventHandler.OnFinish -= Exit;
+            attackCounterResetTimer.OnTimerDone -= ResetAttackCounter;
         }
     }
 }
